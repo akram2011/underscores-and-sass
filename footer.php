603bbb6ace415b64878aa93a7f7b54e872b715e8
asathoor <asathoor@gmail.com>
2017-11-26 22:33:34 +0100

    footers

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'petj-17' ) ); ?>"><?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'petj-17' ), 'WordPress' );
+				printf( esc_html__( 'CMS: %s', 'petj-17' ), 'WordPress' );
 			?></a>
 			<span class="sep"> | </span>
 			<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'petj-17' ), 'petj-17', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( '&copy; Per Thykjaer Jensen 2017. Theme: %1$s based on %2$s.', 'petj-17' ), 'petj-17', '<a href="https://github.com/asathoor/underscores-and-sass">underscores-and-sass</a>' );
 			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
