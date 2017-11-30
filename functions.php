6243c21e9aa9f19516f7290fc9855260e4006b88
Per Thykjaer Jensen <per@multimusen.dk>
2017-11-30 19:46:46 +0100

    improved responive based on layouts

--- functions.php
+++ functions.php
@@ -161,9 +161,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  **/
 $args = array(
 	'flex-width'    => true,
-	'width'         => 980,
 	'flex-height'    => true,
-	'height'        => 200,
 	'default-image' => get_template_directory_uri() . '/images/clouds.jpg',
 );
 add_theme_support( 'custom-header', $args );
